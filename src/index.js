22473cf6498f5f6c1a41540056ba90400bf7a857
JaeHoon Lee <hoonni2709@naver.com>
2021-09-15 03:18:07 +0900

    PWA

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import registerServiceWorker from 'react-service-worker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,4 +11,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+registerServiceWorker().register();
